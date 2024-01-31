f97801c81ae324ded964c9a0b357ac00a6f6ac1c
Klaus Havelund <klaus.havelund@jpl.nasa.gov>
2024-01-31 10:33:45 -0800

    updating CSV reading example

--- demo/demo_using_dictionaries_and_header.py
+++ demo/demo_using_dictionaries_and_header.py
@@ -1,4 +1,3 @@
-from typing import Optional, List
 
 from pycontract import *
 
@@ -7,7 +6,7 @@ class Commands(Monitor):
     def transition(self, event):
         match event:
             case {'op': 'DISPATCH', 'time': time, 'cmd': cmd, 'nr': nr}:
-                return self.DoComplete(time, cmd, nr)
+                return self.DoComplete(int(time), cmd, nr)
 
     @data
     class DoComplete(HotState):
@@ -18,7 +17,7 @@ class Commands(Monitor):
         def transition(self, event):
             match event:
                 case {'op': 'COMPLETE', 'time': time, 'cmd': self.cmd, 'nr': self.nr}:
-                    if time - self.time <= 3000:
+                    if int(time) - self.time <= 3000:
                         return Commands.Completed(self.cmd, self.nr)
                     else:
                         return error(f'{self.cmd} {self.nr} completion takes too long')
@@ -36,17 +35,13 @@ class Commands(Monitor):
                     return error(f'{self.cmd} already completed')
 
 
-def converter(line: list[str]) -> dict:
-    return {'op': line[0], 'time': int(line[1]), 'cmd': line[2], 'nr': line[3]}
-
-
 if __name__ == '__main__':
     m = Commands()
-    csv_reader = CSVReader('commands.csv', converter)
-    for event in csv_reader:
-        if event is not None:
-            m.eval(event)
-    m.end()
-    csv_reader.close()
+    with CSVSource('commands_with_header.csv') as csv_reader:
+        for event in csv_reader:
+            if event is not None:
+                m.eval(event)
+        m.end()
+
 
 
